5fb2f677647b43305e9a1f381385e0835e89ad0a
JulderFab <juldbernal@uan.edu.co>
2022-10-31 23:13:50 -0500

    version 1.0.0 de la aplicacion mascotafeliz

---
+++ src/datasources/mongodb.datasource.ts
@@ -0,0 +1,32 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'mongodb',
+  connector: 'mongodb',
+  url: 'mongodb+srv://julderfab:JoKMongoCluster@clusterciclo4.wlm5sq6.mongodb.net/mascotafelizdb?retryWrites=true&w=majority',
+  host: '',
+  port: 0,
+  user: '',
+  password: '',
+  database: '',
+  useNewUrlParser: true
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class MongodbDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'mongodb';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.mongodb', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
