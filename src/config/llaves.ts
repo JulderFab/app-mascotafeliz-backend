e14273446859f3f73dd73b2c4e41128a82ded8fc
JulderFab <juldbernal@uan.edu.co>
2022-11-14 17:35:20 -0500

    paquetes, metodos y modelo credenciales

---
+++ src/config/llaves.ts
@@ -0,0 +1,4 @@
+export namespace Llaves {
+  export const claveJWT = 'Joker317$';
+  export const urlServicioNotificaciones = 'http://localhost:5000';
+}
--- src/services/autenticacion.service.ts
+++ src/services/autenticacion.service.ts
@@ -1,11 +1,19 @@
 import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {Llaves} from '../config/llaves';
+import {Usuario} from '../models';
+import {UsuarioRepository} from '../repositories';
 const generador = require('password-generator');
 const cryptoJS = require ('crypto-js');
+const jwt = require ('jsonwebtoken');
 
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class AutenticacionService {
-  constructor(/* Add @inject to inject parameters */) {}
+  constructor(
+    @repository(UsuarioRepository)
+    public usuarioRepository : UsuarioRepository
+  ) {}
 
   /*
    * Add service methods here
@@ -18,6 +26,45 @@ export class AutenticacionService {
   CifrarContrasena(contrasena: string) {
     let contrasenaCifrada = cryptoJS.MD5(contrasena).toString();
     return contrasenaCifrada;
+  }
+
+  IdentificarUsuario(usuario: string, contrasena: string) {
+    try {
+      let p = this.usuarioRepository.findOne({where: {correo: usuario, contrasena: contrasena}});
+      if (p) {
+        return p;
+      }
+      return false;
+
+    } catch {
+      return false;
+    }
+  }
+
+  GenerarTokenJWT(usuario: Usuario) {
+    let token = jwt.sign({
+      data: {
+        id: usuario.id,
+        correo: usuario.correo,
+        nombre: usuario.nombre
+      },
+
+    },
+      Llaves.claveJWT);
+
+    return token;
+
+  }
+
+  ValidarTokenJWT (token: string) {
+    try {
+      let datos = jwt.verify(token, Llaves.claveJWT);
+      return datos;
+
+    } catch {
+      return false;
+
+    }
   }
 
 }
